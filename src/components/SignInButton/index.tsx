c206095948a620367ac0605dc8c83f9c969c5f08
zagob <matheusus1_23@hotmail.com>
2021-07-08 00:10:37 -0300

    tests

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,8 +7,6 @@ import styles from './styles.module.scss';
 export function SignInButton() {
     const [session] = useSession();
 
-    console.log(session)
-
     return session ? (
         <button
             type="button"
@@ -16,7 +14,7 @@ export function SignInButton() {
             onClick={() => signOut()}
         >
             <FaGithub color="#04d361" />
-            Matheus Zago
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
---
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import { mocked } from 'ts-jest/utils'
+import { useSession } from 'next-auth/client';
+import { SubscribeButton } from '.';
+
+jest.mock('next-auth/client')
+
+describe('SubscribeButton component', () => {
+    it('renders correctly', () => {
+
+        render(
+            <SubscribeButton />
+        )
+
+        expect(screen.getByText('Subscribe now')).toBeInTheDocument()
+    })
+})
